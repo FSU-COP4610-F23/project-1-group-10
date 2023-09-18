e2599cf00c38b6a6535b51443529d13d3f3c534a
fpgu <105531260+fpgu@users.noreply.github.com>
2023-09-18 19:40:56 -0400

    Add files via upload
    This fixed the first iteration of tilde expansion. There was a misunderstanding of the abilities of strcat(). This commit fixes the problems that arose from the bug.

--- src/envirVar.c
+++ src/envirVar.c
@@ -7,19 +7,7 @@ char *envConvert(char *item)
 
     if(item != NULL)
     {
-        if(*item[0] == '~'){
-    /*        char* temp1 = getenv("HOME");
-            int size = strlen(item) + strlen(temp1) - 1;
-            char* newitem = (char*) malloc(size * sizeof(char))
-            for(int i = 0; i < size; i++){
-                if(i < strlen(temp1)){ newitem[i] = temp1[i]; }
-                else{ newitem[i] = item[i - strlen(temp1) + 1]; }
-            }
-            for(int i = 0; i < strlen(item); i++)*/
-            
-            newEnv = getenv("HOME");
-        }
-        else{ newEnv = getenv(item + 1); }
+        newEnv = getenv(item + 1);
     }
 
     return newEnv;
--- src/shell.c
+++ src/shell.c
@@ -22,7 +22,7 @@ int main()
        
         for(int i = 0; i < tokens->size; i++)
         {
-            if(tokens->items[i][0] == '$' || tokens->items[i][0] == '~')
+            if(tokens->items[i][0] == '$')
             {
                 char *envString =  envConvert(tokens->items[i]);
                 
@@ -32,6 +32,39 @@ int main()
                     tokens->items[i] = strdup(envString);
                 }
             }
+            else if(tokens->items[i][0] == '~')
+            {
+                if(tokens->items[i][1] == '/')
+                {
+                    char *envString = getenv("HOME");
+                    char *envString1 = strdup(envString); 
+                    char *envString2 = ((tokens->items[i]) + 1);
+                    
+                    size_t length = strlen(envString1) + strlen(envString2) + 1;
+                    
+                    char *envString3 = (char *) (malloc(length));
+                    strcpy(envString3, envString1);
+                    char *newString = strcat(envString3, envString2);
+
+                    if(newString)
+                    {
+                        tokens->items[i] = strdup(newString);
+                    }
+
+                    free(envString1);
+                }
+                else
+                {
+                    char *envString = getenv("HOME");
+                    
+                    if(envString)
+                    {
+                        free(tokens->items[i]);
+                        tokens->items[i] = strdup(envString);
+                    }       
+                }                
+            }
+            
         }        
  
         for(int i = 0; i < tokens->size; i++)
